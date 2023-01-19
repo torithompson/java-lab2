ef940a2adbeae4976552162a641568c52be5b2b9
Tori <tori.thompson@student.sl.on.ca>
2023-01-19 17:51:10 -0500

    new file:   res/grades.csv modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
-
+        Scanner fileScanner = new Scanner(new File("res/grades.csv"));
     }
 }
