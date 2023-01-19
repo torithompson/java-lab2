f76f2558dd2416e1e449ec5c4699d8a7371b2d64
Tori <tori.thompson@student.sl.on.ca>
2023-01-19 18:21:12 -0500

    new file:   res/grades.html modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,37 @@
-import java.util.Scanner;
+package src;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner fileScanner = null;
-        fileScanner = new Scanner(new File("res/grades.csv"));
+
+        try {
+            fileScanner = new Scanner(new File("res/grades.csv"));
+            String headerLine = fileScanner.nextLine();
+            headerLine = headerLine.replaceAll("\"", "");// replace quotes with empty string
+            String[] headers = headerLine.split(",");// split the line into an array of strings
+            FileWriter fileWriter = new FileWriter("res/grades.html");
+            StringBuilder builder = new StringBuilder();
+            builder.append("<table>").append("\n");
+            builder.append("<tr>");
+            for (int i = 0; i < headers.length; i++) {
+                builder.append("<th>").append(headers[i]).append("</th>");
+            }
+            builder.append("</tr>").append("\n");
+            builder.append("</table>");
+            fileWriter.write(builder.toString());
+            fileWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
